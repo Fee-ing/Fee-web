4eceba3201fd29616ce5e95feaac7fbf48943181
Fee-ing <2809750987@qq.com>
2019-05-06 15:31:49 +0800

    web update

---
+++ src/config/web.js
@@ -0,0 +1,202 @@
+export const webConfig = {
+  recommend: [
+    {
+      name: 'BT之家',
+      url: 'http://www.btbtt08.com/',
+      icon: 'http://www.btbtt08.com/favicon.ico',
+      desc: 'BT之家是个提供BT下载，bt种子的bt电影下载论坛'
+    },
+    {
+      name: '高清电台',
+      url: 'https://gaoqing.fm/',
+      icon: 'https://gaoqing.fm/favicon.ico',
+      desc: '高清电台每天关注高清电影的720p高清、1080p高清、蓝光原盘高清、高清3d高清、高清mv的磁力链接下载、在线直播、预告片及资讯，是高清电影下载的电影天堂！'
+    },
+    {
+      name: '80s手机电影',
+      url: 'https://www.80ying.com/',
+      icon: 'https://www.80ying.com/favicon.ico',
+      desc: '80s电影网提供最新高清MP4电影、电视剧、动漫下载，采用迅雷磁力链方式下载，免费手机电影电视剧就上80s电影天堂'
+    },
+    {
+      name: 'LOL电影天堂',
+      url: 'http://www.loldytit.com/',
+      icon: 'http://www.loldytit.com/favicon.ico',
+      desc: 'LOL电影天堂最好的迅雷电影下载网，提供最新电影下载、lol电影天堂高清电影迅雷下载和bt种子、最新综艺、动漫、电视剧等下载，最新好看的电影下载首选lol电影天堂！'
+    },
+    {
+      name: '大狗狗影视',
+      url: 'http://www.dagou2018.com/',
+      icon: '',
+      desc: '大狗狗影视提供最全的2019最新电影，好看的电视剧以及各种电影电视剧排行榜、最新电视剧、动漫及综艺的在线观看，每天第一时间更新，放送最新好看的电影电视剧。'
+    },
+    {
+      name: '电影天堂',
+      url: 'https://www.dytt8.net/',
+      icon: 'https://www.dytt8.net/favicon.ico',
+      desc: '最好的迅雷电影下载网，分享最新电影，高清电影、综艺、动漫、电视剧等下载！'
+    },
+    {
+      name: '阳光电影天堂',
+      url: 'https://www.ygdy8.com/',
+      icon: '',
+      desc: '最好的迅雷电影下载网，分享最新电影，高清电影、综艺、动漫、电视剧等下载！'
+    },
+    {
+      name: '爱看电影',
+      url: 'https://www.aiktt.com/',
+      icon: 'https://www.aiktt.com/favicon.ico',
+      desc: '爱看影视天堂为各位网友提供：1080p高清电影BT种子下载，720p高清电影BT种子下载'
+    },
+    {
+      name: '电影港',
+      url: 'http://www.dygang.net/',
+      icon: 'http://www.dygang.net/favicon.ico',
+      desc: '电影港-每天搜集最新的电影，高清电影，720p高清电影，1080p高清电影，的免费迅雷下载。专注于高清电影的下载服务。'
+    },
+    {
+      name: '高清电影网',
+      url: 'http://gaoqing.la/',
+      icon: 'http://gaoqing.la/favicon.ico',
+      desc: '中国高清网是每天关注提供720p高清、1080p高清、蓝光原盘高清、高清3d高清、高清mv最新热门bt种子磁力链迅雷下载网站，是下载了解高清电影天堂！'
+    },
+    {
+      name: '影视网址导航',
+      url: 'http://allyingshi.com/',
+      icon: 'http://allyingshi.com/favicon.ico',
+      desc: '人人影视网址大全，这里为您搜集了优秀的影视资源下载站和在线观看站，以及字幕下载站，片源站，请长期保存'
+    },
+    {
+      name: '盘搜搜（百度云）',
+      url: 'http://www.pansoso.com/',
+      icon: 'http://www.pansoso.com/favicon.ico',
+      desc: '盘搜搜网盘搜索引擎支持百度云搜索、115网盘、360云盘、华为网盘、新浪微盘等网盘搜索服务，是您工作、学习、娱乐的最佳百度云搜索神器。'
+    },
+    {
+      name: '字幕库（字幕）',
+      url: 'http://www.zimuku.la/',
+      icon: 'http://www.zimuku.la/images/v2/favicon.png',
+      desc: '字幕，字幕下载，电影字幕下载，美剧字幕下载，韩剧字幕下载，中文字幕下载'
+    },
+    {
+      name: '六一电影（在线播放）',
+      url: 'http://www.61dy.com/',
+      icon: 'http://www.61dy.com/favicon.ico',
+      desc: '六一电影网提供最新电影最新电视剧，接受任何网友的求片，指在为您提供体验最好电影体验！'
+    }
+  ],
+  btSearch: [
+    {
+      name: '磁力站',
+      url: 'http://oabt005.com/home.html',
+      icon: 'http://oabt005.com/favicon.ico',
+      desc: 'MAG磁力站 - 磁力下载站，cili001.com，最新美剧下载，日剧下载，电影下载，韩剧下载'
+    },
+    {
+      name: '种子磁力',
+      url: 'http://zhongzicilibt.net/',
+      icon: 'http://zhongzicilibt.net/favicon.ico',
+      desc: '种子磁力 - 全球收录BT种子和磁力链接资源数量最多、资源更新以及搜索速度最快、运行最稳定、功能最强大的BT种子磁力链接搜索引擎，目前世界上最好用的BT种子磁力链接搜索引擎,拥有全球最大的BT种子磁力链接数据库。高清电影、pdf图书、流行动漫、连续剧、无损音乐、综艺节目、美女图片、免费软件、各种教程、单机游戏等资源应有尽有。'
+    },
+    {
+      name: 'DiggBT',
+      url: 'http://diggbtcn.ws/',
+      icon: 'http://diggbtcn.ws/static/images/favicon.ico',
+      desc: 'DiggBT- 专注于从DHT网络抓取BT种子和磁力链接，目前索引了数千万BT种子元信息(metainfo)，是宇宙BT种子资源最丰富的BT种子搜索下载、磁力链接下载的专业网站,你可以在这里搜索和下载电影、剧集、音乐、图书、图片、综艺、软件、动漫、教程、游戏等资源，DiggBT可以说是一款BT种子搜索神器、P2P种子搜索神器、万能BT种子搜索下载神器、磁力链接搜索下载。本站本身不存储提供BT种子下载，仅提供从DHT网络抓取的磁力链接以及其他网站提供的下载链接。'
+    },
+    {
+      name: 'Zooqle',
+      url: 'https://zooqle.com/',
+      icon: 'https://zooqle.com/img/zq-favicon32.png',
+      desc: 'Download torrents at Zooqle'
+    },
+    {
+      name: 'skrBT',
+      url: 'https://www.skrbt.com/',
+      icon: 'https://www.skrbt.com/favicon.ico',
+      desc: 'SkrBT - 专注于从DHT网络爬取种子和磁力链接,目前索引了上千万的磁力链接,资源涵盖了电影、剧集、音乐、图书、图片、综艺、软件、动漫、教程、游戏等领域,是全银河系资源最丰富的种子搜索、磁力链接搜索专业网站。'
+    },
+    {
+      name: 'RARBG Torrents',
+      url: 'https://rarbgprx.org/torrents.php',
+      icon: 'https://rarbgprx.org/favicon.ico',
+      desc: 'Torrents , movies , download , music , games , free , RARBG Torrents , filmi , download'
+    },
+    {
+      name: '838888磁力链接',
+      url: 'https://www.868888.net/',
+      icon: 'https://www.868888.net/static/img/favicon.ico',
+      desc: '磁力搜索(838888)是一个专业提供DHT磁力资源搜索和下载的网站，索引全网上千万BT种子的磁力链接来提供搜索，又被誉为BT种子搜索神器。也是国内最大的磁力链接的发现和分享社区。'
+    },
+    {
+      name: '磁力搜索导航',
+      url: 'https://lai.yuweining.cn/archives/909/#menu_index_2',
+      icon: 'https://lai.yuweining.cn/favicon.ico',
+      desc: '磁力搜索网站汇总'
+    },
+  ],
+  others: [
+    {
+      name: '韩饭网（韩剧、韩综）',
+      url: 'https://www.hanfan.cc/',
+      icon: 'https://www.hanfan.cc/favicon.ico',
+      desc: '韩饭网 - 是一家专注于韩娱分享的中文网站，内容包括：韩国娱乐新闻报道、k-pop、MV、韩剧、综艺节目、时尚画报写真图片等明星资讯。'
+    },
+    {
+      name: '韩剧网（韩剧、在线播放）',
+      url: 'https://www.hanjuwang.com/',
+      icon: 'https://www.hanjuwang.com/favicon.ico',
+      desc: '提供最新韩剧、韩国电影、韩国综艺、韩娱新闻、韩剧海报剧照、韩国明星资料、韩剧OST、韩剧剧情及韩剧收视率等的介绍和评论，找韩剧就上韩剧网。'
+    },
+    {
+      name: '人人影视（美剧）',
+      url: 'http://www.zmz2019.com/',
+      icon: 'http://www.zmz2019.com/favicon.ico',
+      desc: '字幕组网站是由爱好者成员成立的网站，继续为您翻译最新最快的海外影视剧字幕，美剧，日剧，电影最新字幕下载'
+    },
+    {
+      name: '一站搜（美剧）',
+      url: 'http://videos.yizhansou.com/',
+      icon: '',
+      desc: '一站搜专注于美剧搜索，方便大家愉快地观看, 请大家记住一站搜。'
+    },
+    {
+      name: '追新番（日剧）',
+      url: 'http://www.zhuixinfan.com/main.php',
+      icon: 'http://www.zhuixinfan.com/favicon.ico',
+      desc: '日剧'
+    },
+    {
+      name: '嘀哩嘀哩（动漫、在线播放）',
+      url: 'http://www.dilidili.name/',
+      icon: 'http://www.dilidili.name/favicon.ico',
+      desc: '嘀哩嘀哩，故被网友称为d站以及dilidili，无论您喜欢那种类型，嘀哩嘀哩都会筛选经典作品并将新作经典轮番推荐，相信嘀哩嘀哩都能为您一网打尽，为您带来一场美妙的动漫盛宴，这里是兴趣使然的嘀哩嘀哩。'
+    },
+    {
+      name: '风之动漫（漫画）',
+      url: 'https://www.fzdm.com/',
+      icon: 'https://www.fzdm.com/favicon.ico',
+      desc: '风之动漫 - 在线漫画 日本动漫 火影忍者漫画 海贼王漫画'
+    },
+  ],
+  vip: [
+    {
+      name: '船长解析',
+      url: 'http://czjx8.com/',
+      icon: '',
+      desc: '本站提供vip视频在线免费解析，vip视频解析，优酷vip解析，爱奇艺vip解析，腾讯vip解析，乐视vip解析，芒果vip解析服务！'
+    },
+    {
+      name: 'VIP解析',
+      url: 'http://v.kkwen.com/',
+      icon: 'http://v.kkwen.com/images/tx.png',
+      desc: 'VIP视频解析站,爱奇艺VIP免费看,腾讯VIP视频免费看,优酷VIP视频免费看,乐视,搜狐,芒果,PPTV,华数等各VIP视频免费看'
+    },
+    {
+      name: '全民解析',
+      url: 'http://tool.liumingye.cn/video/',
+      icon: 'http://tool.liumingye.cn/favicon.ico',
+      desc: '本站为广大网友提供优酷VIP解析，爱奇艺VIP解析，腾讯VIP解析，乐视VIP解析，芒果VIP解析等解析服务，让你省去购买视频VIP费用，欢迎大家收藏本站，并将它介绍给您的朋友！'
+    },
+  ]
+}
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,31 +1,78 @@
 <template>
   <div class="page">
-    
+    <div class="title">推荐网站</div>
+    <div class="web-wrapper">
+      <div class="web" v-for="(item, index) in webConfig.recommend" :key="index" :title="item.desc">
+        <span class="icon" v-if="item.icon" :style="{ backgroundImage: 'url(' + item.icon + ')'}"></span>
+        <a :href="item.url" target="_blank">{{item.name}}</a>
+      </div>
+    </div>
+    <div class="title">磁力搜索</div>
+    <div class="web-wrapper">
+      <div class="web" v-for="(item, index) in webConfig.btSearch" :key="index" :title="item.desc">
+        <span class="icon" v-if="item.icon" :style="{ backgroundImage: 'url(' + item.icon + ')'}"></span>
+        <a :href="item.url" target="_blank">{{item.name}}</a>
+      </div>
+    </div>
+    <div class="title">其他分类</div>
+    <div class="web-wrapper">
+      <div class="web" v-for="(item, index) in webConfig.others" :key="index" :title="item.desc">
+        <span class="icon" v-if="item.icon" :style="{ backgroundImage: 'url(' + item.icon + ')'}"></span>
+        <a :href="item.url" target="_blank">{{item.name}}</a>
+      </div>
+    </div>
+    <div class="title">VIP解析</div>
+    <div class="web-wrapper">
+      <div class="web" v-for="(item, index) in webConfig.vip" :key="index" :title="item.desc">
+        <span class="icon" v-if="item.icon" :style="{ backgroundImage: 'url(' + item.icon + ')'}"></span>
+        <a :href="item.url" target="_blank">{{item.name}}</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { webConfig } from '@/config/web'
+
 export default {
   name: 'Index',
-  components: {
-    
-  },
   data () {
     return {
-      
+      webConfig
     }
-  },
-  methods: {
-    
-  },
-  mounted () {
-    
   }
 }
 </script>
 
 <style lang="less" scoped>
 .page{
-  
+  width: 1000px;
+  margin: 0 auto;
+  .title{
+    font-size: 16px;
+    font-weight: 600;
+    text-align: center;
+    margin: 30px 0;
+  }
+  .web-wrapper{
+    display: flex;
+    flex-wrap: wrap;
+    .web{
+      width: 25%;
+      display: flex;
+      align-items: center;
+      font-size: 14px;
+      margin-bottom: 20px;
+      .icon{
+        display: block;
+        width: 15px;
+        height: 15px;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        margin-right: 5px;
+      }
+    }
+  }
 }
 </style>
